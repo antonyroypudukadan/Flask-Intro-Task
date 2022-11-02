1d6be54e2dcd1c999abb3f129a814dbd4f1888b2
antonyroypudukadan <116718131+antonyroypudukadan@users.noreply.github.com>
2022-11-02 14:59:08 +0530

    Add files via upload

--- FlaskMarket/app.py
+++ FlaskMarket/app.py
@@ -1,19 +1,16 @@
-from flask import Flask, render_template, redirect, request, url_for, jsonify, make_response
-# from flask_pymongo import PyMongo
+from flask import Flask, render_template, redirect, request, url_for, g, jsonify, make_response
 import pymongo
 import jwt
 import datetime
 from functools import wraps
-import bcrypt
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'Flask'
 
-
 client = pymongo.MongoClient("mongodb+srv://m001-student:m001-mongodb-basics@sandbox.frwq2ks.mongodb.net/?retryWrites=true&w=majority")
 db = client.antony_test
 c = db.calculator
-
+b=db.blacklist
 
 @app.route("/")
 @app.route("/home")
@@ -26,15 +23,23 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.args.get('token')
-        if not token:
-            return jsonify({'message' : 'Token is missing!'}), 401
+        g.token = token
+        f = b.find_one({'btoken': token})
+        # try:
+        if not f:
+            if not token:
+                return jsonify({'message': 'Token is missing!'}), 401
 
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithm=["HS256"])
-        except:
-            return jsonify({'message' : 'Token is invalid'}), 401
+            try:
+                data = jwt.decode(token, app.config['SECRET_KEY'], algorithm=["HS256"])
+            except:
+                return jsonify({'message': 'Token is invalid'}), 401
 
-        return f(*args, **kwargs)
+            return f(*args, **kwargs)
+        else:
+            return jsonify({'message': 'Token blacklisted'}), 401
+        # except:
+        #     return jsonify({'message': 'Token blacklisted'}), 401
     return decorated
 
 @app.route('/calculator')
@@ -50,32 +55,30 @@ def protected():
 @app.route('/login')
 def login():
     auth = request.authorization
+    print(vars(request))
     if auth and auth.password:
         x = c.find_one({'user':auth.username})
 
         try:
             if x['user'] == auth.username and x['password'] == auth.password:
                 token = jwt.encode({'user': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['SECRET_KEY'], algorithm="HS256")
-                myquery = {'user': auth.username}
-                newvalues = {"$set": {"token": token}}
-                c.update_one(myquery, newvalues)
             return token
-            return jsonify({'token': jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])})
         except:
             return make_response('Wrong username and password', 401,{'WWW-Authenticate': 'Basic realm="Login Required"'})
     return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
-    auth = request.authorization
+    if request.method == 'POST':
+        user = request.form['name']
+        password = request.form['password']
+        c.insert_one({'user': user, 'password': password})
+    return render_template('register.html')
 
-    if auth and auth.password:
-        token = jwt.encode({'user': auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'],algorithm="HS256")
-        # token = str(token)
-        c.insert_one({'user': auth.username, 'password': auth.password, 'token': token})
-        return token
-        return jsonify({'token' : jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])})
-    return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+@app.route("/logout/<token>")
+def logout(token):
+    b.insert_one({'btoken':token})
+    return jsonify({'message': 'Logged Out'})
 
 
 if __name__ == '__main__':
